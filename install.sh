4b8beae69e1e1b0dfdb921dee4d702433baf1954
nandunkz <nandunkz07@gmail.com>
2021-03-12 13:16:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,4 +34,111 @@ rm user_extend
 rm user_list
 rm user_lock
 rm user_unlock
-wget 
+cd
+cd menu
+mv accounts /usr/local/sbin/accounts
+mv base_ports /usr/local/sbin/base_ports
+mv base_ports_wc /usr/local/sbin/base_ports-wc
+mv base_script /usr/local/sbin/base_script
+mv clear_cache /usr/local/sbin/clear_cache
+mv connections /usr/local/sbin/connections
+mv create /usr/local/sbin/create
+mv create_random /usr/local/sbin/create_random
+mv create_trial /usr/local/sbin/create_trial
+mv delete_expired /usr/local/sbin/delete_expired
+mv diagnose /usr/local/sbin/diagnose
+mv edit_dropbear /usr/local/sbin/edit_dropbear
+mv edit_openssh /usr/local/sbin/edit_openssh
+mv edit_openvpn /usr/local/sbin/edit_openvpn
+mv edit_ports /usr/local/sbin/edit_ports
+mv edit_squid3 /usr/local/sbin/edit_squid3
+mv edit_stunnel4 /usr/local/sbin/edit_stunnel4
+mv locked_list /usr/local/sbin/locked_list
+mv menu /usr/local/sbin/menu
+mv options /usr/local/sbin/options
+mv reboot_sys /usr/local/sbin/reboot_sys
+mv reboot_sys_auto /usr/local/sbin/reboot_sys_auto
+mv restart_service /usr/local/sbin/restart_service
+mv server /usr/local/sbin/server
+mv set_multilogin_autokill /usr/local/sbin/set_multilogin_autokill
+mv set_multilogin_autokill_lib /usr/local/sbin/set_multilogin_autokill_lib
+mv show_ports /usr/local/sbin/show_ports
+mv user_delete /usr/local/sbin/user_delete
+mv user_details /usr/local/sbin/user_details
+mv user_details_lib /usr/local/sbin/user_details_lib
+mv user_extend /usr/local/sbin/user_extend
+mv user_list /usr/local/sbin/user_list
+mv user_lock /usr/local/sbin/user_lock
+mv user_unlock /usr/local/sbin/user_unlock
+cd /usr/local/sbin
+chmod +x accounts
+
+chmod +x base-ports
+
+chmod +x base-ports-wc
+
+chmod +x base script
+
+chmod +x clear-cache
+
+chmod +x connections
+
+chmod +x create
+
+chmod +x create_random
+
+chmod +x create_trial
+
+chmod +x delete_expired
+
+chmod +x diagnose
+
+chmod +x edit_dropbear
+
+chmod +x edit_openssh
+
+chmod +x edit_openvpn
+
+chmod +x edit_ports
+
+chmod +x edit_squid3
+
+chmod +x edit_stunnel4
+
+chmod +x locked_list
+
+chmod +x menu
+
+chmod +x options
+
+chmod +x reboot_sys
+
+chmod +x reboot_sys_auto
+
+chmod +x restart_service
+
+chmod +x server
+
+chmod +x set_multilogin_autokill
+
+chmod +x set_multilogin_autokill_lib
+
+chmod +x show_ports
+
+chmod +x user_delete
+
+chmod +x user_details
+
+chmod +x user_details_lib
+
+chmod +x user_extend
+
+chmod +x user_list
+
+chmod +x user_lock
+
+chmod +x user_unlock
+
+cd
+
+exit

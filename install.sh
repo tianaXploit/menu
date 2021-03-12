1bbcd8d899e8c67513262c5cd756c60f31178fe7
nandunkz <nandunkz07@gmail.com>
2021-03-12 12:58:16 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cd /usr/local/sbin
+rm accounts
+rm base-ports
+rm base-ports-wc
+rm base script
+rm clear-cache
+rm connections
+rm create
+rm create_random
+rm create_trial
+rm delete_expired
+rm diagnose
+rm edit_dropbear
+rm edit_openssh
+rm edit_openvpn
+rm edit_ports
+rm edit_squid3
+rm edit_stunnel4
+rm locked_list
+rm menu
+rm options
+rm reboot_sys
+rm reboot_sys_auto
+rm restart_service
+rm server
+rm set_multilogin_autokill
+rm set_multilogin_autokill_lib
+rm show_ports
+rm user_delete
+rm user_details
+rm user_details_lib
+rm user_extend
+rm user_list
+rm user_lock
+rm user_unlock
+wget 
